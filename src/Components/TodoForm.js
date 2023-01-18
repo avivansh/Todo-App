d5ad0fa10b9e3ad1ddf3f5862d0eaa5d92a47a0e
vanshmittal <vanshmittal1299@gmail.com>
2023-01-18 22:33:05 +0530

    sending a dispatch

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -6,8 +6,29 @@ import { TodoContext } from "../Context/TodoContext";
 import { ADD_TODO } from "../Context/action.types";
 
 const TodoForm = () => {
+  const [todoString, setTodoString] = useState("");
+  const { dispatch } = useContext(TodoContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoString === "") {
+      return alert("Please enter a todo");
+    }
+
+    const todo = {
+      todoString,
+      id: v4(),
+    };
+
+    dispatch({
+      type: ADD_TODO,
+      payload: todo,
+    });
+
+    setTodoString("");
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <InputGroup>
           <Input
@@ -15,6 +36,8 @@ const TodoForm = () => {
             name="todo"
             id="todo"
             placeholder="Your next todo"
+            value={todoString}
+            onChange={(e) => setTodoString(e.target.value)}
           />
           <Button color="warning"> ADD</Button>
         </InputGroup>
